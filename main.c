b0f8905e671e19a7cc8fdb5c90cacf8b514bb3d2
spamela <nunezpam@gmail.com>
2014-01-29 09:58:47 -0500

    updated main.c

--- main.c
+++ main.c
@@ -5,10 +5,12 @@
 
 
 int main(int* argc, char** argv) {
-    IntList* list =  mk_list(void);
+    IntList* list = mk_list(void);
     print_list(list);
-    is_empty(list);
-
+    printf("is_empty(list) = 1? %d\n", is_empty(list));
+    free_list(list);
+    IntList* list = mk_list(void);
+    
     return 0;
 }
 
